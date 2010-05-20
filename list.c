1f5084276154da8fc69ed0f9e402d3a164d2955f
Rob Pilling <robpilling@gmail.com>
2010-05-20 20:10:00 +0100

    Initial working list.{c,h}

--- list.c
+++ list.c
@@ -1,12 +1,142 @@
-#include <stdlib.h>
+#include <stdlib.h> /* free() only */
+#include <setjmp.h> /* alloc.h */
+
 #include "list.h"
+#include "alloc.h"
 
-struct list *list_new(char *);
+struct list *list_new(char *d)
+{
+  struct list *l = umalloc(sizeof(struct list));
+  l->data = d;
+  l->next = l->prev = NULL;
+  return l;
+}
 
-int  list_insert(struct list *, char *); /* inserts char * before the list * */
-int  list_append(struct list *, char *); /* inserts char * after the list * */
+/* inserts char * before the list * */
+void list_insertbefore(struct list *l, char *d)
+{
+  if(l->prev){
+    struct list *next = l->next;
 
-struct list *list_extract(struct list *); /* removes the list * from its list and returns it */
-void list_remove(struct list *); /* as above, but frees the extract'd */
+    l->next = list_new(d);
+    l->next->next = next;
+    l->next->prev = l;
+  }else if(l->data){
+    /* append a node, but then swap the data around */
+    char *tmp;
+    list_insertafter(l, d);
 
-void list_free(struct list *);
+    tmp = l->data;
+    l->data = l->next->data;
+    l->next->data = tmp;
+  }else
+    /* l doesn't have prev, nor ->data. therefore, tis head */
+    l->data = d;
+}
+
+void list_insertafter(struct list *l, char *d)
+{
+  if(l->data){
+    struct list *next = l->next;
+
+    l->next = list_new(d);
+    l->next->prev = l;
+    l->next->next = next;
+    next->prev = l->next;
+  }else
+    l->data = d;
+}
+
+/* inserts char * at the very end of the list */
+void list_append(struct list *l, char *d)
+{
+  if(!l->data){
+    l->data = d;
+    return;
+  }
+
+  while(l->next)
+    l = l->next;
+
+  l->next = list_new(d);
+  l->next->prev = l;
+}
+
+/* removes the list * from its list and returns it */
+char *list_extract(struct list *l)
+{
+  char *ret = l->data;
+
+  if(l->next){
+    struct list *delthis = l->next;
+    l->data = l->next->data;
+    l->next = l->next->next;
+
+    free(delthis);
+  }else if(l->prev){
+    free(l);
+    l->prev->next = NULL;
+  }else{
+    l->data = NULL;
+  }
+
+  return ret;
+}
+
+/* as above, but frees the extract'd */
+void list_remove(struct list *l)
+{
+  free(list_extract(l));
+}
+
+int list_count(struct list *l)
+{
+  int i;
+  l = list_gethead(l);
+
+  if(!l->next && !l->data)
+    return 0;
+
+  i = 1;
+  while(l->next)
+    l = l->next, ++i;
+  return i;
+}
+
+struct list *list_gethead(struct list *l)
+{
+  while(l->prev)
+    l = l->prev;
+  return l;
+}
+
+struct list *list_gettail(struct list *l)
+{
+  while(l->next)
+    l = l->next;
+  return l;
+}
+
+struct list *list_getindex(struct list *l, int i)
+{
+  l = list_gethead(l);
+  while(i --> 0 && l->next)
+    l = l->next;
+  if(i > 0)
+    return NULL;
+  return l;
+}
+
+void list_free(struct list *l)
+{
+  struct list *del;
+
+  l = list_gethead(l);
+
+  while(l){
+    del = l;
+    l = l->next;
+    free(del->data);
+    free(del);
+  }
+}
--- list.h
+++ list.h
@@ -5,13 +5,21 @@ struct list
 {
   char *data;
   struct list *next, *prev;
-} list_new(char *);
+} *list_new(char *);
 
-int  list_insert(struct list *, char *); /* inserts char * before the list * */
-int  list_append(struct list *, char *); /* inserts char * after the list * */
+void list_insertbefore(struct list *, char *); /* inserts char * before the list * */
+void list_insertafter(struct list *, char *);  /* inserts char * after the list * */
 
-struct list *list_extract(struct list *); /* removes the list * from its list and returns it */
+void list_append(struct list *, char *); /* inserts char * at the very end of the list */
+
+char *list_extract(struct list *); /* removes the list * from its list and returns it */
 void list_remove(struct list *); /* as above, but frees the extract'd */
+
+int list_count(struct list *);
+
+struct list *list_gethead(struct list *);
+struct list *list_gettail(struct list *);
+struct list *list_getindex(struct list *, int);
 
 void list_free(struct list *);
 
