dcf84535d4e3eda6dad2563efc653b8c0fd52879
Rob Pilling <robpilling@gmail.com>
2010-05-21 16:16:09 +0100

    Update list functions header

--- list.h
+++ list.h
@@ -12,8 +12,15 @@ void list_insertafter(struct list *, char *);	/* inserts char * after the list *
 
 void list_append(struct list *, char *); /* inserts char * at the very end of the list */
 
-char *list_extract(struct list *); /* removes the list * from its list and returns it */
-void list_remove(struct list *); /* as above, but frees the extract'd */
+char        *list_extract(struct list *); /* removes the list * from its list and returns it */
+void         list_remove(struct list *); /* as above, but frees the extract'd */
+struct list *list_extract_range(struct list **, int);
+void         list_remove_range(struct list **, int);
+
+/*
+ * list_extract returns the extracted list, and adjusts the pointer
+ * passed to it, so that it points to the top of the new list
+ */
 
 int list_count(struct list *);
 
