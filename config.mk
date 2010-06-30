11b788afcd7b040c0e9a597f344b3f555ca28161
Rob Pilling <robpilling@gmail.com>
2010-06-30 02:29:25 +0100

    Moved LDFLAGS

--- config.mk
+++ config.mk
@@ -5,11 +5,14 @@ VERBOSE = 0
 CC			?= gcc
 LD			= gcc
 
+LDFLAGS = -g -lncurses
+
+
 ifeq (${CC},gcc)
 	CFLAGS	= -g -pipe -W -Wall -Wcast-align -Wcast-qual -Wshadow -Wnested-externs \
 			-Waggregate-return -Wbad-function-cast -Wpointer-arith -Wcast-align \
 			-Wwrite-strings -Wstrict-prototypes -Wmissing-prototypes -Winline \
 			-Wno-char-subscript -Wredundant-decls -Wextra -pedantic -ansi -O1
 else
-	CFLAGS	= -Wimplicit-function-declaration -Wunsupported -Wwrite-strings -Wall
+	CFLAGS	= -g -Wimplicit-function-declaration -Wunsupported -Wwrite-strings -Wall
 endif
