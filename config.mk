2d32279869786f475e67d05016f99bef2b245ed9
Rob Pilling <rob pilling at gmail>
2011-06-04 19:07:12 +0100

    -D_GNU_SOURCE

--- config.mk
+++ config.mk
@@ -1,5 +1,5 @@
 WARN_EXTRA = -Wno-unused-parameter -Wno-char-subscripts
-MACROS     = -D_POSIX_SOURCE
+MACROS     = -D_POSIX_SOURCE -D_GNU_SOURCE
 
 CC      = cc
 CFLAGS  = -g -Wall -Wextra -pedantic -std=c99 ${MACROS} ${WARN_EXTRA}
